7481eab2b6577217de19091163342047b4134bf6
siham <siham.heddi.dev@gmail.com>
2024-04-28 21:42:02 +0100

    get head departement when we fetch all departements

--- src/departement/departement.module.ts
+++ src/departement/departement.module.ts
@@ -5,9 +5,10 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { Departement } from './entities/departement.entity';
 import { AuthModule } from 'src/auth/auth.module';
 import { JwtModule } from '@nestjs/jwt';
+import { TeacherModule } from 'src/teacher/teacher.module';
 
 @Module({
-  imports: [AuthModule, SequelizeModule.forFeature([Departement]), JwtModule],
+  imports: [TeacherModule, AuthModule, SequelizeModule.forFeature([Departement]), JwtModule],
   controllers: [DepartementController],
   providers: [DepartementService],
   exports: [DepartementService],
--- src/departement/departement.service.ts
+++ src/departement/departement.service.ts
@@ -3,17 +3,29 @@ import { CreateDepartementDto } from './dto/create-departement.dto';
 import { UpdateDepartementDto } from './dto/update-departement.dto';
 import { InjectModel } from '@nestjs/sequelize';
 import { Departement } from './entities/departement.entity';
+import { TeacherService } from 'src/teacher/teacher.service';
 
 @Injectable()
 export class DepartementService {
   constructor( @InjectModel(Departement)
-    private readonly departementModel: typeof Departement,) {}
+  private readonly departementModel: typeof Departement,
+  private readonly TeacherService: TeacherService) { }
   async create(createDepartementDto: CreateDepartementDto) {
     return await this.departementModel.create(createDepartementDto);
   }
 
- async findAll(limit: number=10) {
-    return await this.departementModel.findAll({limit});
+  async findAll(limit: number = 10) {
+    const departements = await this.departementModel.findAll({ limit });
+    const new_departements = [];
+    
+    for (let i = 0; i < departements.length; i++) {
+      const departement: any = departements[i]?.dataValues;
+      const teacher = await this.TeacherService.findOne(departement.head_departement_id);
+      departement.head_departement_name = teacher.first_name + " " + teacher.last_name;
+      new_departements.push(departement);
+    }
+
+    return new_departements;
   }
 
   async findOne(id: number): Promise<Departement> {
